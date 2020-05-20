ea8f92a8f3466c4998915fc4dbc73a992dfac561
WiggidyW <user@thinkpad.home>
2020-05-19 19:45:25 -0500

    viable row stream!

--- web_requests/imdb_dataset/src/core.rs
+++ web_requests/imdb_dataset/src/core.rs
@@ -9,8 +9,8 @@ use std::{
 };
 use bytes::Bytes;
 
-const DELIMITER: u8 = b'\t';
-const TERMINATOR: u8 = b'\n';
+const CSV_DELIMITER: u8 = b'\t';
+const CSV_TERMINATOR: u8 = b'\n';
 
 struct Response {
 	inner: reqwest::Response,
@@ -26,21 +26,22 @@ struct DecodedStream {
 
 struct CsvRowStream {
 	inner: DecodedStream,
-	buf: bytes::buf::IntoIter,
+	row_buf: Vec<u8>,
+	chunk_buf: bytes::buf::IntoIter<Bytes>,
 }
 
 impl Response {
 	fn into_raw_stream(self) -> RawStream {
-		RawStream { inner:
-			Box::pin(self.inner.bytes_stream())
+		RawStream {
+			inner: Box::pin(self.inner.bytes_stream()),
 		}
 	}
 }
 
 impl RawStream {
 	fn into_decoded_stream(self) -> DecodedStream {
-		DecodedStream { inner:
-			GzipDecoder::new(self)
+		DecodedStream {
+			inner: GzipDecoder::new(self),
 		}
 	}
 }
@@ -48,7 +49,7 @@ impl RawStream {
 // We are using the 'RawStream' as a NewType to convert the reqwest error into an io error for this trait.
 // In doing this, we become eligible for GzipDecoder.
 //
-// Inner is pinned, and we are pinning it again. This is due to auto-derives.
+// Inner is pinned, and we are pinning it again. This is due to auto-derives on the Stream trait.
 // An alternative to the double pinning would be nice.
 //
 // https://docs.rs/async-compression/0.3.4/async_compression/stream/struct.GzipDecoder.html
@@ -61,5 +62,54 @@ impl Stream for RawStream {
 			// https://docs.rs/crate/reqwest/0.10.4/source/src/error.rs
 			Some(Err(err)) => Poll::Ready(Some(Err(io::Error::new(io::ErrorKind::Other, err)))),
 		}
+	}
+}
+
+impl DecodedStream {
+	fn into_csv_row_stream(self) -> CsvRowStream {
+		CsvRowStream::new(self)
+	}
+}
+
+impl CsvRowStream {
+	fn new(stream: DecodedStream) -> Self {
+		Self {
+			inner: stream,
+			row_buf: Vec::new(),
+			chunk_buf: bytes::buf::IntoIter::new(Bytes::new()),
+		}
+	}
+
+	fn take(&mut self) -> Bytes {
+		(&mut self.row_buf).drain(..)
+			.collect()
+	}
+
+	fn next(&mut self) -> Option<Bytes> {
+		while let Some(b) = (&mut self.chunk_buf).next() {
+			(&mut self.row_buf).push(b);
+			if b == CSV_TERMINATOR {
+				return Some(self.take());
+			}
+		}
+		None
+	}
+}
+
+impl Stream for CsvRowStream {
+	type Item = Result<Bytes, std::io::Error>;
+	fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+		match self.next() {
+			Some(b) => Poll::Ready(Some(Ok(b))),
+			None =>
+		match futures_core::ready!(Stream::poll_next(Pin::new(&mut (&mut self.inner).inner), cx)) {
+			None => Poll::Ready(None),
+			Some(Err(err)) => Poll::Ready(Some(Err(err))),
+			Some(Ok(chunk)) => {
+		self.chunk_buf = bytes::buf::IntoIter::new(chunk);
+		match self.next() {
+			Some(b) => Poll::Ready(Some(Ok(b))),
+			None => Poll::Ready(None),
+		}},},}
 	}
 }
