3b1815348df3ea39adbd5b07b1243a3c84003158
WiggidyW <user@thinkpad.home>
2020-06-10 20:38:12 -0500

    pad

--- file_server/src/microservice/microservice.rs
+++ file_server/src/microservice/microservice.rs
@@ -8,28 +8,58 @@ use super::{FileContent, Content, Error};
 
 type Result<T> = std::result::Result<T, Error>;
 
-impl<'r, 'db, 't, 'm> FromParam<'r> for FileId<'db, 't, 'm> {
-    type Error = &'r RawStr;
-    fn from_param(param: &'r RawStr) -> Result<Self, Self::Error> {
-        if let Ok(i) = u32::from_param(param)
+enum Id {
+    Alias(String),
+    Id(u32),
+}
+struct States<'db, 't, 'm> {
+    database: &'db Database,
+    file_table: &'t FileTable,
+    file_map: &'m FileMap,
+}
+
+impl Id {
+    fn into_file_id<'db, 't, 'm>(
+        &self,
+        states: &States<'db, 't, 'm>,
+    ) -> Result<FileId<'db, 't, 'm>>
+    {
+        match self {
+            Self::Alias(s) => FileId::from_alias(
+                    s,
+                    states.database,
+                    states.file_table,
+                    states.file_map,
+                )
+                .map_err(|e| Error::internal(e))?
+                .ok_or(Error::alias_not_found()),
+            Self::Id(i) => FileId::from_id(
+                    *i,
+                    states.database,
+                    states.file_table,
+                    states.file_map,
+                )
+                .map_err(|e| Error::internal(e))?
+                .ok_or(Error::alias_not_found()),
+        }
+    }
+    fn into_key(self) -> String {
+        match self {
+            Self::Alias(s) => s,
+            Self::Id(i) => i.to_string(),
+        }
     }
 }
 
-#[derive(Deref)]
-#[deref(forward)]
-struct Id<'db, 't, 'm> {
-    inner: FileId<'db, 't, 'm>
+impl<'r> FromParam<'r> for Id {
+    type Error = &'r RawStr;
+    fn from_param(param: &'r RawStr) -> StdResult<Self, Self::Error> {
+        if let Ok(i) = u32::from_param(param) {
+            return Ok(Id::Id(i));
+        }
+        String::from_param(param).map(|s| Id::Alias(s))
+    }
 }
-
-// impl<'a, 'r, 'db, 't, 'm> FromRequest<'a, 'r> for Id<'db, 't, 'm> {
-//     type Error = Error;
-//     fn from_request(
-//         request: &'a Request<'r>,
-//     ) -> request::Outcome<Self, Self::Error>
-//     {
-
-//     }
-// }
 
 pub fn run<'db: 'static, 't: 'static, 'm: 'static>(
     database: &'db Database,
@@ -45,221 +75,111 @@ pub fn run<'db: 'static, 't: 'static, 'm: 'static>(
 }
 
 #[get("/list")]
-fn list<'db, 't, 'm>(states: State<States>) -> Result<Content> {
-    states.list_ids_and_aliases()
+fn list(states: State<States>) -> Result<Content> {
+    let list = FileId::all(states.database, states.file_table, states.file_map)
+        .map_err(|e| Error::internal(e))?
+        .into_iter()
+        .map(|id| Ok((
+            *id,
+            id.get_aliases()?
+        )))
+        .collect::<StdResult<Vec<(u32, Vec<String>)>, core::Error>>()
+        .map_err(|e| Error::internal(e))?;
+    Ok(Content::alias_list(list.into_iter()))
 }
 
 #[get("/files/<id>")]
-fn get_file_id<'t>(
-    id: u32,
+fn get_file<'t>(
+    id: Id,
     states: State<States<'_, 't, '_>>,
 ) -> Result<FileContent<'t>>
 {
-    let id = Id::from_id(id, &states)?;
-    id.stream_file()
-}
-
-#[get("/files/<alias>", rank = 2)]
-fn get_file_alias<'t>(
-    alias: String,
-    states: State<States<'_, 't, '_>>,
-) -> Result<FileContent<'t>>
-{
-    let id = Id::from_alias(&alias, &states)?;
-    id.stream_file()
+    let file = id.into_file_id(&states)?
+        .ro_file()
+        .map_err(|e| Error::internal(e))?;
+    match FileContent::new(file) {
+        Err(e) => Err(Error::internal(e)),
+        Ok(None) => Err(Error::file_not_found()),
+        Ok(Some(f)) => Ok(f),
+    }
 }
 
 #[get("/streams/<id>")]
-fn get_streams_id(id: u32, states: State<States>) -> Result<Content> {
-    let id = Id::from_id(id, &states)?;
-    id.display_stream_hashes(*id)
-}
-
-#[get("/streams/<alias>", rank = 2)]
-fn get_streams_alias(alias: String, states: State<States>) -> Result<Content> {
-    let id = Id::from_alias(&alias, &states)?;
-    id.display_stream_hashes(alias)
+fn get_stream_hashes(id: Id, states: State<States>) -> Result<Content> {
+    let fid = id.into_file_id(&states)?;
+    match fid.ro_file()
+        .map_err(|e| Error::internal(e))?
+        .stream_hashes()
+    {
+        Err(e) => return Err(Error::internal(e)),
+        Ok(None) => (),
+        Ok(Some(h)) => return Ok(Content::stream_hashes(
+            iter::once((id.into_key(), h))
+        ))
+    };
+    match fid.rw_file()
+        .map_err(|e| Error::internal(e))?
+        .refresh_stream_hashes()
+    {
+        Err(e) => Err(Error::internal(e)),
+        Ok(None) => Err(Error::file_not_found()),
+        Ok(Some(h)) => Ok(Content::stream_hashes(
+            iter::once((id.into_key(), h))
+        ))
+    }
 }
 
 #[get("/probe/<id>")]
-fn get_probe_id(id: u32, states: State<States>) -> Result<Content> {
-    let id = Id::from_id(id, &states)?;
-    id.json_probe()
-}
-
-#[get("/probe/<alias>", rank = 2)]
-fn get_probe_alias(alias: String, states: State<States>) -> Result<Content> {
-    let id = Id::from_alias(&alias, &states)?;
-    id.json_probe()
+fn get_probe_id(id: Id, states: State<States>) -> Result<Content> {
+    match id.into_file_id(&states)?
+        .rw_file()
+        .map_err(|e| Error::internal(e))?
+        .json_probe()
+    {
+        Err(e) => Err(Error::internal(e)),
+        Ok(None) => Err(Error::file_not_found()),
+        Ok(Some(json)) => Ok(Content::json_probe(json)),
+    }
 }
 
 #[get("/aliases/<id>")]
-fn get_aliases_id(id: u32, states: State<States>) -> Result<Content> {
-    let id = Id::from_id(id, &states)?;
-    id.display_aliases(*id)
-}
-
-#[get("/aliases/<alias>", rank = 2)]
-fn get_aliases_alias(alias: String, states: State<States>) -> Result<Content> {
-    let id = Id::from_alias(&alias, &states)?;
-    id.display_aliases(alias)
+fn get_aliases(id: Id, states: State<States>) -> Result<Content> {
+    match id.into_file_id(&states)?
+        .get_aliases()
+    {
+        Err(e) => Err(Error::internal(e)),
+        Ok(a) => Ok(Content::alias_list(iter::once((id.into_key(), a)))),
+    }
 }
 
 #[post("/aliases/<id>?push", format = "json", data = "<list>")]
-fn push_aliases_id(
-    id: u32,
+fn push_aliases(
+    id: Id,
     list: Json<Vec<String>>,
     states: State<States>,
 ) -> Result<Content>
 {
-    let id = Id::from_id(id, &states)?;
-    id.push_aliases(list)
-}
-
-#[post("/aliases/<alias>?push", format = "json", data = "<list>", rank = 2)]
-fn push_aliases_alias(
-    alias: String,
-    list: Json<Vec<String>>,
-    states: State<States>,
-) -> Result<Content>
-{
-    let id = Id::from_alias(&alias, &states)?;
-    id.push_aliases(list)
+    match id.into_file_id(&states)?
+        .with_aliases(list.into_inner())
+    {
+        Err(e) => Err(Error::internal(e)),
+        Ok(None) => Err(Error::invalid_alias_addition()),
+        Ok(Some(())) => Ok(Content::okay()),
+    }
 }
 
 #[post("/aliases/<id>?pop", format = "json", data = "<list>")]
-fn pop_aliases_id(
-    id: u32,
+fn pop_aliases(
+    id: Id,
     list: Json<Vec<String>>,
     states: State<States>,
 ) -> Result<Content>
 {
-    let id = Id::from_id(id, &states)?;
-    id.pop_aliases(list)
-}
-
-#[post("/aliases/<alias>?pop", format = "json", data = "<list>", rank = 2)]
-fn pop_aliases_alias(
-    alias: String,
-    list: Json<Vec<String>>,
-    states: State<States>,
-) -> Result<Content>
-{
-    let id = Id::from_alias(&alias, &states)?;
-    id.pop_aliases(list)
-}
-
-struct States<'db, 't, 'm> {
-    database: &'db Database,
-    file_table: &'t FileTable,
-    file_map: &'m FileMap,
-}
-
-impl<'db, 't, 'm> States<'db, 't, 'm> {
-    fn list_ids_and_aliases(&self) -> Result<Content> {
-        let list = FileId::all(self.database, self.file_table, self.file_map)
-            .map_err(|e| Error::internal(e))?
-            .into_iter()
-            .map(|id| Ok((*id, id.get_aliases()?)))
-            .collect::<StdResult<Vec<(u32, Vec<String>)>, core::Error>>()
-            .map_err(|e| Error::internal(e))?;
-        Ok(Content::alias_list(list.into_iter()))
-    }
-}
-
-impl<'db, 't, 'm> Id<'db, 't, 'm> {
-    fn from_id(
-        id: u32,
-        states: &States<'db, 't, 'm>,
-    ) -> Result<Self>
+    match id.into_file_id(&states)?
+        .without_aliases(list.into_inner())
     {
-        match FileId::from_id(
-            id,
-            states.database,
-            states.file_table,
-            states.file_map,
-        ) {
-            Err(e) => Err(Error::internal(e)),
-            Ok(None) => Err(Error::id_not_found()),
-            Ok(Some(i)) => Ok(Self { inner: i }),
-        }
-    }
-    fn from_alias(alias: &str, states: &States<'db, 't, 'm>) -> Result<Self> {
-        match FileId::from_alias(
-            alias,
-            states.database,
-            states.file_table,
-            states.file_map,
-        ) {
-            Err(e) => Err(Error::internal(e)),
-            Ok(None) => Err(Error::alias_not_found()),
-            Ok(Some(i)) => Ok(Self { inner: i }),
-        }
-    }
-    fn stream_file(&self) -> Result<FileContent<'t>> {
-        let file = self.inner.ro_file().map_err(|e| Error::internal(e))?;
-        match FileContent::new(file) {
-            Err(e) => Err(Error::internal(e)),
-            Ok(None) => Err(Error::file_not_found()),
-            Ok(Some(f)) => Ok(f),
-        }
-    }
-    fn display_stream_hashes(
-        &self,
-        display: impl Serialize + Hash + Eq,
-    ) -> Result<Content>
-    {
-        let file = self.inner.ro_file().map_err(|e| Error::internal(e))?;
-        // Try to get the hashes from the read-only file
-        match file.stream_hashes() {
-            Err(e) => return Err(Error::internal(e)),
-            Ok(None) => (),
-            Ok(Some(h)) => return Ok(Content::stream_hashes(
-                iter::once((display, h))
-            )),
-        };
-        // Get the hashes from the read-write file
-        let mut file = self.inner.rw_file().map_err(|e| Error::internal(e))?;
-        match file.refresh_stream_hashes() {
-            Err(e) => Err(Error::internal(e)),
-            Ok(None) => Err(Error::file_not_found()),
-            Ok(Some(h)) => Ok(Content::stream_hashes(
-                iter::once((display, h))
-            )),
-        }
-    }
-    fn display_aliases(
-        &self,
-        display: impl Serialize + Hash + Eq,
-    ) -> Result<Content>
-    {
-        match self.inner.get_aliases() {
-            Err(e) => Err(Error::internal(e)),
-            Ok(a) => Ok(Content::alias_list(iter::once((display, a)))),
-        }
-    }
-    fn push_aliases(&self, list: Json<Vec<String>>) -> Result<Content> {
-        match self.inner.with_aliases(list.into_inner()) {
-            Err(e) => Err(Error::internal(e)),
-            Ok(None) => Err(Error::invalid_alias_addition()),
-            Ok(Some(())) => Ok(Content::okay()),
-        }
-    }
-    fn pop_aliases(&self, list: Json<Vec<String>>) -> Result<Content> {
-        match self.inner.without_aliases(list.into_inner()) {
-            Err(e) => Err(Error::internal(e)),
-            Ok(None) => Err(Error::invalid_alias_removal()),
-            Ok(Some(())) => Ok(Content::okay()),
-        }
-    }
-    fn json_probe(&self) -> Result<Content> {
-        match self.inner.rw_file()
-            .map_err(|e| Error::internal(e))?
-            .json_probe()
-        {
-            Err(e) => Err(Error::internal(e)),
-            Ok(None) => Err(Error::file_not_found()),
-            Ok(Some(json)) => Ok(Content::json_probe(json)),
-        }
+        Err(e) => Err(Error::internal(e)),
+        Ok(None) => Err(Error::invalid_alias_removal()),
+        Ok(Some(())) => Ok(Content::okay()),
     }
 }
